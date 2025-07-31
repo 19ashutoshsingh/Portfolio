7fdd3be67ab11af2ef65392d54ce9ee9f7d900b5
19ashutoshsingh <154892951+19ashutoshsingh@users.noreply.github.com>
2025-07-31 11:01:51 +0530

    Initialized the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', // This enables the dark mode feature
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
